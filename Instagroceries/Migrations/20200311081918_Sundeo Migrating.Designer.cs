30e9a8c64d5b11052644ade70b0d26cb96432473
[sundeo] <[sundaydeogratias8@gmail.com]>
2020-03-30 18:38:34 +0300

    Finished migrating to new database

--- Instagroceries/Migrations/20200311081918_Sundeo Migrating.Designer.cs
+++ Instagroceries/Migrations/20200311081918_Sundeo Migrating.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Instagroceries.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Instagroceries.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200311081918_Sundeo Migrating")]
+    partial class SundeoMigrating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
